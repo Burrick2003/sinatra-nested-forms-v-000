1d34a2dc907c09574be10b100c0aca1b3e154f65
Blakeley Bond <burrick2003@yahoo.com>
2018-02-22 18:31:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-        params[:pirate][:ships].each do |shipstuff|
-          Ship.new(shipstuff)
+        params[:pirate][:ships].each do |details|
+          Ship.new(details)
         end
       @ships=Ship.all
 
